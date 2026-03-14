bf028578d8eed617b1bd4f522dbad5ca10a14119
J0onas <zorrillalucajhonathan@gmail.com>
2026-03-14 04:17:14 -0500

    bug de login

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,22 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // 2. Le decimos a NestJS que procese las cookies entrantes
+  // ¡EL FIX PARA EL ERROR 404! Le decimos que todo empieza con /api/v1
+  app.setGlobalPrefix('api/v1');
+  
   app.use(cookieParser());
   
-  // 3. El nuevo candado de CORS (Solo deja pasar a tu entorno local y a tu Vercel)
   app.enableCors({
     origin: [
-      'http://localhost:3000', // Tu frontend cuando desarrollas en tu PC
-      'https://saas-erp-frontend-dxmzj3zfr-j0onas-projects.vercel.app' // Tu frontend en producción en Vercel
+      'http://localhost:3000', 
+      'https://saas-erp-frontend-dxmzj3zfr-j0onas-projects.vercel.app'
     ],
-    credentials: true, // <-- VITAL: Esta es la llave que permite que la cookie httpOnly viaje
+    credentials: true, // Vital para las cookies
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   
-  // Usamos process.env.PORT para que Render asigne el puerto dinámicamente, o 3000 en local
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
