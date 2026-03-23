ea0a8134af50426d6b0537875fd96feea96a3443
J0onas <zorrillalucajhonathan@gmail.com>
2026-03-23 01:44:50 -0500

    arreglo bug 129

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { PlansModule } from '../plans/plans.module';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
+  imports: [PlansModule],
+  exports: [ProductService],
 })
 export class ProductModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PlansModule } from '../plans/plans.module';
 
 @Module({
     controllers: [UsersController],
     providers: [UsersService],
+    imports: [PlansModule],
     exports: [UsersService],
 })
 export class UsersModule {}
